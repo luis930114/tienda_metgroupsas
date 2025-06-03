8932028ec0aa74dea8b2d07647f6bbcbc94a4663
luis930114 <luis930114@gmail.com>
2025-06-03 08:44:00 -0500

    feat: CRUD store

--- controllers/store_controller.py
+++ controllers/store_controller.py
@@ -10,3 +10,28 @@ def create_store(name):
     db.session.commit()
     return jsonify({"id": store.id, "name": store.name, "items": []})
 
+def get_store(name):
+    store = StoreModel.query.filter_by(name=name).first()
+    if store:
+        return jsonify({"id": store.id, "name": store.name, "items": [
+            {"id": item.id, "name": item.name, "price": item.price, "store_id": item.store_id} for item in store.items
+        ]})
+    return {"message": "Store not found"}, 404
+
+def delete_store(name):
+    store = StoreModel.query.filter_by(name=name).first()
+    if store:
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted."}
+    return {"message": "Store not found"}, 404
+
+def get_all_stores():
+    stores = StoreModel.query.all()
+    return jsonify({"stores": [
+        {"id": store.id, "name": store.name, "items": [
+            {"id": item.id, "name": item.name, "price": item.price, "store_id": item.store_id} for item in store.items
+        ]} for store in stores
+    ]})
+
+
--- routes/store_routes.py
+++ routes/store_routes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint
-from controllers.store_controller import create_store
+from controllers.store_controller import create_store, get_store, delete_store, get_all_stores
 
 bp = Blueprint('store', __name__)
 
 bp.route('/store/<string:name>', methods=['POST'])(create_store)
+bp.route('/store/<string:name>', methods=['GET'])(get_store)
+bp.route('/store/<string:name>', methods=['DELETE'])(delete_store)
+bp.route('/stores', methods=['GET'])(get_all_stores)
+
